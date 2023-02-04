23c2c601324f4f3c0ce2e8d37bacec44c4009a76
Samuel Grave <samuel.grave.silva@outlook.com>
2023-02-04 19:40:50 -0300

    refactors photo_organizer and create smaller methods

--- lib/photo_organizer.rb
+++ lib/photo_organizer.rb
@@ -1,4 +1,4 @@
-require 'awesome_print'
+# frozen_string_literal: true
 
 require_relative 'photo'
 
@@ -7,31 +7,45 @@ class PhotoOrganizer
 
   def initialize
     @file_name = ARGV[0] || 'input.txt'
+    @photos_result = []
   end
 
-  def run
-    file = File.open(@file_name)
-    files = file.read.split("\n")
-    photos = files.map do |f|
-      Photo.new(f)
-    end
-    photos_by_city = photos.sort_by(&:timestamp).group_by(&:city_name)
+  def call
+    open_file
+    map_photos
+    map_photos_by_city
+    create_photo_new_name
 
-    puts photos_by_city['Krakow'].length
-    photos_result = []
-    photos.each do |photo|
-      photos_on_city = photos_by_city[photo.city_name]
+    puts @photos_result.join("\n")
+  end
+
+  private
+
+  def open_file
+    file = File.open(@file_name)
+    @files = file.read.split("\n")
+  end
+
+  def map_photos
+    @photos = @files.map { |file| Photo.new(file) }
+  end
+
+  def map_photos_by_city
+    @photos_by_city = @photos.sort_by(&:timestamp).group_by(&:city_name)
+  end
+
+  def create_photo_new_name
+    @photos.each do |photo|
+      photos_on_city = @photos_by_city[photo.city_name]
       digits_on_length = photos_on_city.length.to_s.length
-      ap digits_on_length
+
       index = photos_on_city.find_index(photo) + 1
 
       photo.new_name(index, digits_on_length)
 
-      photos_result << photo.to_s
+      @photos_result << photo.to_s
     end
-
-    puts photos_result.join("\n")
   end
 end
 
-PhotoOrganizer.new.run
+PhotoOrganizer.new.call
