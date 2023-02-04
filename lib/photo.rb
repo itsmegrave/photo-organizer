f6cd8f7f0fd8df671dfd388b151e906086e8bbe6
Samuel Grave <grave@Samuels-MacBook-Pro.local>
2023-02-04 11:50:02 -0300

    implements the Photo Class

---
+++ lib/photo.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'time'
+
+class Photo
+  attr_accessor :formatted_photo_name
+
+  def initialize(photo_data)
+    @original_data = photo_data
+  end
+
+  def extension
+    File.extname(parsed_data[0])
+  end
+
+  def city_name
+    parsed_data[1]
+  end
+
+  def timestamp
+    Time.new(parsed_data[2])
+  end
+
+  def add_formatted_photo_name(index, zero_mask)
+    "#{city_name}#{zero_mask}#{index}#{extension}"
+  end
+
+  private
+
+  def parsed_data
+    @parsed_data ||= @original_data.split(', ')
+  end
+end
